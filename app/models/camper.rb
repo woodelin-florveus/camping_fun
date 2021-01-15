4472ecc5a57ee97ed3b8e045e0d2e86477e0dd08
Sam Hall <mann.samantha11@gmail.com>
2021-01-15 12:01:00 -0500

    push two

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -2,6 +2,13 @@ class Camper < ApplicationRecord
     has_many :signups 
     has_many :activities, through: :signups
 
+    # def all_activities
+    #     self.signups.each do |signup|
+    #         signup.activity_id.each do |activity|
+    #             activity.name
+    #         end
+    #     end
+    # end
 
 end
 
